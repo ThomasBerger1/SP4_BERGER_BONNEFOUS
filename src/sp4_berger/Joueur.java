d33f1a829a3c30eca493641422b58a8e432a69e2
alizeebonnefous <alizeebonnefous@localhost>
2022-11-18 17:12:40 +0100

    TP2
    1. Création d'une première classe et quelques objets associés
    achevement des questions jusqu'à la question 10.
    Soit initialisation des variables et affichage de l'étiquette de la 
    bière 1 et la bière 2
    ajout d'un constructeur
    Rajout de 3 autres bières
    affichage de l'etat de la bière avec la  méthode toString()
    rajout de quelques autre biere et affichage de leur etat
    
    2. Un convertisseur de températures : version classes
    création du nouveau projet
    ajout de la classe convertisseur
    classe de convertisseur complétée (attributs + methodes signifiées)
    création de 2 nouveau objet Convert1 et Convert2
    affichage du nombre de conversions pour chaque objet à l'aide d'un scanner
    afficahge d'un menu + permission du choix de la conversion et de la valeur associé par l'utilisateur
    
    3. Manipulation des objets par leur ref
    création de 3 objets assiette 1,2 et 3
    création d'un tableau vide de 10 cases
    
    4.
    initialisation du nombre de voitures possédées à 0
    ajout 2eme voiture bob et 2eme et 3eme a reno
    mise en place du boléen "ajouter_voiture"
    ajout mott clé this
    
    TP3
    
    ...
    
    
    Projet puissance 4

---
+++ src/sp4_berger/Joueur.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_berger;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author alizeebonnefous
+ */
+public class Joueur {
+    
+    String Nom;
+    String Couleur;
+    ArrayList <Jeton> reserveJetons= new ArrayList <Jeton> ();
+    
+
+
+    public Joueur(String unNom) {
+        Nom = unNom;
+    }   
+    
+    public void affecterCouleur(String UneCouleur){
+        Couleur= UneCouleur;
+    }
+    
+    public void ajouterJeton(Jeton UnJeton) {
+        reserveJetons.add(UnJeton);
+    }
+    
+    
+
+   
+//NombreDeJetons ()
+//return reserveJeton.size
+}
